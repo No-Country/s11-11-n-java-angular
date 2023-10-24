f77b0925ab704fee46f7cc4f27df03463b14455d
andres_betancur <fabetancur10@misena.edu.co>
2023-10-23 21:56:32 -0500

    ruteo login

--- frontend/src/app/pages/auth/pages/login/layout-login.component.ts
+++ frontend/src/app/pages/auth/pages/login/layout-login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { LoginService } from '../../../../services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,6 +16,30 @@ export class LayoutLoginComponent {
       Validators.minLength(8),
     ]),
   });
+
+  constructor(
+    private loginService: LoginService,
+    private router: Router
+  ) {}
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  login() {
+    this.loginService.Login(this.loginForm.value).subscribe((res: any) => {
+      this.loginService.id = res.id;
+      this.loginService.token = res.token;
+      localStorage.setItem('id', res.id);
+      localStorage.setItem('token', res.token);
+      this.router.navigate(['/home']);
+    });
+    console.log(this.loginForm.value);
+    console.log('conexion completada  redirigido a home');
+  }
 
   onSubmit() {
     if (this.loginForm.valid) {
