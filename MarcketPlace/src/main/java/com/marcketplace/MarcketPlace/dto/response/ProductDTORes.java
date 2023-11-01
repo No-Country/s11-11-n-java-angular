923c92cd7d2dd1973fa9bc1b0746a662a771c14f
DamianCanteros <109114128+DamianCanteros@users.noreply.github.com>
2023-10-31 22:19:38 -0300

    Update ProductDTORes.java

--- MarcketPlace/src/main/java/com/marcketplace/MarcketPlace/dto/response/ProductDTORes.java
+++ MarcketPlace/src/main/java/com/marcketplace/MarcketPlace/dto/response/ProductDTORes.java
@@ -1,6 +1,5 @@
 package com.marcketplace.MarcketPlace.dto.response;
 
-import com.marcketplace.MarcketPlace.model.Customers;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +13,7 @@ public class ProductDTORes {
     private String name;
     private Double price;
     private String images;
-    private Customers seller;
+    private CustomerRegistration seller;
     private CategoryDTORes category;
     private String shippingStatus;
     private int stock;
